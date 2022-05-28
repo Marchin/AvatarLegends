962ddbd0ec353b9954c71e92ff2132af7975ed48
Martín Monterrosa <mmonterrosa94@gmail.com>
2022-05-28 00:16:54 -0300

    Add ability to remove campaigns

--- Assets/Scripts/Popups/CampaignSelectionPopup.cs
+++ Assets/Scripts/Popups/CampaignSelectionPopup.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class CampaignSelectionPopup : Popup {
     [SerializeField] private ButtonList _campaignsList = default;
     [SerializeField] private Button _addCampaign = default;
+    [SerializeField] private Button _removeCampaigns = default;
     [SerializeField] private Button _closeButton = default;
     private AppData Data => ApplicationManager.Instance.Data;
     private UserData UserData => Data.User;
@@ -12,6 +13,7 @@ public class CampaignSelectionPopup : Popup {
 
     private void Awake() {
         _closeButton.onClick.AddListener(PopupManager.Instance.Back);
+        _removeCampaigns.onClick.AddListener(RemoveCampaigns);
         _addCampaign.onClick.AddListener(async () => {
             var addCampaignPopup = await PopupManager.Instance.GetOrLoadPopup<AddCampaignPopup>(restore: false);
             addCampaignPopup.Populate(
@@ -44,6 +46,45 @@ public class CampaignSelectionPopup : Popup {
             });
         }
         _campaignsList.Populate(buttons);
+    }
+
+    private async void RemoveCampaigns() {
+        var listPopup = await PopupManager.Instance.GetOrLoadPopup<ListPopup>();
+        List<InformationData> infoList = new List<InformationData>(UserData.Campaigns.Count);
+        List<string> campaignsToRemove = new List<string>(UserData.Campaigns.Count);
+        Refresh();
+
+        void Refresh() {
+            infoList.Clear();
+            foreach (var campaign in UserData.Campaigns) {
+                infoList.Add(new InformationData {
+                    Content = campaign.Key,
+                    IsToggleOn = campaignsToRemove.Contains(campaign.Key),
+                    OnToggle = on => {
+                        if (on) {
+                            campaignsToRemove.Add(campaign.Key);
+                        } else {
+                            campaignsToRemove.Remove(campaign.Key);
+                        }
+
+                        Refresh();
+                    },
+                    OnHoverIn = () => TooltipManager.Instance.ShowMessage(campaign.Value.Description),
+                    OnHoverOut = TooltipManager.Instance.Hide
+                });
+            }
+            
+            listPopup.Populate(() => infoList,
+                "Remove Campaigns",
+                () => {
+                    foreach (var campaign in campaignsToRemove) {
+                        UserData.Campaigns.Remove(campaign);
+                    }
+
+                    RefreshCampaignButtons();
+                }
+            );
+        }
     }
 
     public override object GetRestorationData() {
