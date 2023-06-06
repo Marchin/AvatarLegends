6b3993b8699cf0ee352aa39b91d55e76aa038901
Marchin <mmonterrosa94@gmail.com>
2023-06-05 23:55:16 -0300

    Fix tooltip positions when resizing the window

--- Assets/Scripts/Managers/TooltipManager.cs
+++ Assets/Scripts/Managers/TooltipManager.cs
@@ -1,8 +1,10 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TooltipManager : MonoBehaviourSingleton<TooltipManager> {
     [SerializeField] private RectTransform _tooltipContainer = default;
+    [SerializeField] private CanvasScaler _canvasScaler = default;
     [SerializeField] private TextMeshProUGUI _tooltipMsg = default;
     [SerializeField] private float _tooltipPad = default;
     private readonly float _halfHeight = 0.5f * Screen.height;
@@ -26,18 +28,17 @@ public class TooltipManager : MonoBehaviourSingleton<TooltipManager> {
 
     private void Update() {
         Vector2 pos = Input.mousePosition;
-        Vector2 offset = Vector3.up * (_tooltipPad + (_tooltipContainer.rect.height / 2f));
+        Vector2 tooltipsHalfSize = _tooltipContainer.rect.size * _tooltipContainer.lossyScale * 0.5f;
+        Vector2 offset = Vector3.up * (_tooltipPad + tooltipsHalfSize.y);
 
         if (pos.y <= _halfHeight) {
             pos += offset;
         } else {
             pos -= offset + (_cursorHeight * Vector2.up);
         }
-        
-        float halfWidth = _tooltipContainer.rect.width * 0.5f;
-        float halfHeight = _tooltipContainer.rect.height * 0.5f;
-        pos.x = Mathf.Clamp(pos.x, halfWidth, Screen.width - halfWidth);
-        pos.y = Mathf.Clamp(pos.y, halfHeight, Screen.height - halfHeight);
+        ;
+        pos.x = Mathf.Clamp(pos.x, tooltipsHalfSize.x, Screen.width - tooltipsHalfSize.x);
+        pos.y = Mathf.Clamp(pos.y, tooltipsHalfSize.y, Screen.height - tooltipsHalfSize.y);
         _tooltipContainer.position = pos;
     }
 }
